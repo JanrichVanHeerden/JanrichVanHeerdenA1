3d6f4e609bac4018614e054a685eada773925362
JanrichVanHeerden <jbvanheerden@hotmail.com>
2016-09-07 17:18:14 +1000

    finish add data to list function

--- JanrichvanHeerdenA1.py
+++ JanrichvanHeerdenA1.py
@@ -7,48 +7,60 @@
 # Q- Quit
 
 def main():
-    menu_options = ["r","c","a","m","q"]
+    menu_options = ["r", "c", "a", "m", "q"]
     print("Welcome to Shopping list by Janrich van Heerden")
     print("items loaded from items")
-    print("Menu:\nR - List required items\nC - List completed items\nA - add new item\nM - Mark an item completed\nQ - Quit")
+    print(
+        "Menu:\nR - List required items\nC - List completed items\nA - add new item\nM - Mark an item completed\nQ - Quit")
     menu_choice = menu(menu_options)
     while menu_choice in menu_options:
         if menu_choice == "r":
             print("r is good")
-            #list required items
+            # list required items
         elif menu_choice == "c":
             print("c is good")
-            #list completed items
+            # list completed items
         elif menu_choice == "a":
             print("a is good")
-            #add new item
+            # add new item
         elif menu_choice == "m":
             print("m is good")
-            #list required items
+            # list required items
 
 
 def menu(menu_options):
     menu_choice = str(input(">>> ").lower())
     while menu_choice not in menu_options:
-        menu_choice=str(input("Please enter a valid input, r,c,a,m,q"))
+        menu_choice = str(input("Please enter a valid input, r,c,a,m,q"))
     return menu_choice
 
-#def required_items():
-#
+
+# def required_items():
+
+# this function will add data to a list
 def add_data_to_list():
-  item_name_list = []
-  price_list = []
-  priority_list = []
-  required_or_completed_list = []
-  item_file = open("item.csv", "r")
-  
+    item_name_list = []
+    price_list = []
+    priority_list = []
+    required_or_completed_list = []
+    item_file = open("item.csv", "r")
+    for line in item_file:
+        item_name, item_price, item_priority, required_or_completed = line.split(",")
+        required_or_completed = required_or_completed[-4:-1]  # thanks to michael for showing me this formatting trick
+        item_name_list.append(item_name)
+        priority_list.append(item_priority)
+        price_list.append(item_price)
+        required_or_completed_list.append(required_or_completed)
+    item_file.close()
+    return item_name_list, price_list, priority_list, required_or_completed_list
 
-#def completed_items():
+
+
+# def completed_items():
 #
-#def add_new_item():
+# def add_new_item():
 #
-#def mark_complete():
+# def mark_complete():
 #
-#def compile_new_list():
+# def compile_new_list():
 main()
-
