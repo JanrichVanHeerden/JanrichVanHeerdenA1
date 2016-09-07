7cc02d4cad377d882667cc56d53eb1e6a798ddd2
JanrichVanHeerden <jbvanheerden@hotmail.com>
2016-09-07 16:34:39 +1000

    add menu, add menu error checking

--- JanrichvanHeerdenA1.py
+++ JanrichvanHeerdenA1.py
@@ -10,30 +10,37 @@ def main():
     menu_options = ["r","c","a","m","q"]
     print("Welcome to Shopping list by Janrich van Heerden")
     print("items loaded from items")
-    print("Menu:\nR - List required items\nC - List completed items\n A - add new item\nM - Mark an item completed\n Q - Quit")
-    menu_choice=str(input("").lower())
+    print("Menu:\nR - List required items\nC - List completed items\nA - add new item\nM - Mark an item completed\nQ - Quit")
+    menu_choice=str(input(">>> ").lower())
     while menu_choice in menu_options:
         if menu_choice == "r":
+            print("r is good")
             #list required items
         elif menu_choice == "c":
+            print("c is good")
             #list completed items
         elif menu_choice == "a":
+            print("a is good")
             #add new item
         elif menu_choice == "m":
+            print("m is good")
             #list required items
+            menu_choice = menu(menu_choice,menu_options)
+
 def menu(menu_choice,menu_options):
     while menu_choice not in menu_options:
         menu_choice=str(input("Please enter a valid input, r,c,a,m,q"))
     return menu_choice
 
-def required_items():
-
-def list_data():
-
-def completed_items():
-
-def add_new_item():
-
-def mark_complete():
-
-def compile_new_list():
+#def required_items():
+#
+#def list_data():
+#
+#def completed_items():
+#
+#def add_new_item():
+#
+#def mark_complete():
+#
+#def compile_new_list():
+main()
